798c65df51dfafff7db96628a83f261fc00344ba
Kaitlyn Genius <kaitlyn.genius@louisville.edu>
2024-12-01 12:01:22 -0500

    calculator.py: add multiply and divide operations
    Signed-off-by: Kaitlyn Genius <kaitlyn.genius@louisville.edu>

--- calculator.py
+++ calculator.py
@@ -8,18 +8,30 @@ def add():
 def subtract():
     return randrange(1000) - randrange(1000)
 
+def divide():
+    return randrange(1000) / randrange(1000)
+
+def multiply():
+    return randrange(1000) * randrange(1000)
+
 def main():
     seed(0)
 
     print("Welcome to the [not-so] random number calculator!")
 
-    op = randrange(2)
+    op = randrange(4)
     if op == 0:
         print("Adding!")
         result = add()
     elif op == 1:
         print("Subtracting!")
         result = subtract()
+    elif op == 2:
+        print("Dividing!")
+        result = divide()
+    elif op == 3:
+        print("Multiplying!")
+        result = multiply()
 
     print(f"The result is {result}")
 
